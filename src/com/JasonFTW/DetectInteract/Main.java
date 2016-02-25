d3efe45f467b4c49ca65a067bb9ee76e25c424e1
Jason-FTW <jyu031103@gmail.com>
2016-02-24 18:50:25 -0800

    Main class and plugin.yml

---
+++ src/com/JasonFTW/DetectInteract/Main.java
@@ -0,0 +1,68 @@
+package com.JasonFTW.DetectInteract;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+
+	@Override
+	public void onEnable(){
+		loadConfiguration();
+		getLogger().info("Config Found, loading!");
+		
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	@Override
+	public void onDisable(){
+
+	}
+
+	File configFile = new File(this.getDataFolder(), "config.yml");
+	FileConfiguration users = YamlConfiguration.loadConfiguration(configFile);
+
+	public void loadConfiguration(){
+		//creating defaults
+		getConfig().options().copyDefaults(true); 
+		saveConfig();
+		
+		String[] defaultItems = {"257", "270", "274", "278", "285"};
+		
+		String path = "Settings.Items";
+		getConfig().set(path, Arrays.asList(defaultItems));
+		getConfig().addDefault("", "#Items support IDs only.");
+		getConfig().addDefault("", "Get ids from: http://minecraft-ids.grahamedgecombe.com/");
+		getConfig().addDefault("", "Created by JasonFTW");
+		getConfig().addDefault(path, "");
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+	
+	public String getItems(){
+		for(Object i : getConfig().getList("Settings.Items")){
+			String s = (String)i;
+			return s;
+		}
+		return null;
+	}
+	
+	@EventHandler
+	public void playerInteract(PlayerInteractEvent e){
+		if(e.getItem().equals(Material.getMaterial(getItems())) || e.getItem() != null || getItems() != null){
+			if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+				getServer().getLogger().info("Player " + e.getPlayer() + " RIGHT CLICKED with item " + e.getItem());
+			}
+			else if(e.getAction().equals(Action.LEFT_CLICK_AIR) || e.getAction().equals(Action.LEFT_CLICK_BLOCK)){
+				getServer().getLogger().info("Player " + e.getPlayer() + " LEFT CLICKED with item " + e.getItem());
+			}
+		}
+	}
+}
