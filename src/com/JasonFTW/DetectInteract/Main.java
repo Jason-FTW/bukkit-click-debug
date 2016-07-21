6d1310ad7651cb8e3de95135a5778ac2c44c50ca
Jason-FTW <jyu031103@gmail.com>
2016-07-21 12:08:02 -0700

    Updated main.java

--- src/com/JasonFTW/DetectInteract/Main.java
+++ src/com/JasonFTW/DetectInteract/Main.java
@@ -1,7 +1,6 @@
 package com.JasonFTW.DetectInteract;
 
 import java.io.File;
-import java.util.Arrays;
 
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -31,32 +30,33 @@ public class Main extends JavaPlugin implements Listener{
 
 	public void loadConfiguration(){
 		//creating defaults
+		this.saveDefaultConfig();
 		getConfig().options().copyDefaults(true); 
 		saveConfig();
 		
-		String[] defaultItems = {"257", "270", "274", "278", "285"};
+		//String[] defaultItems = {"257", "270", "274", "278", "285"};
 		
-		String path = "Settings.Items";
-		getConfig().set(path, Arrays.asList(defaultItems));
-		getConfig().addDefault("", "#Items support IDs only.");
-		getConfig().addDefault("", "Get ids from: http://minecraft-ids.grahamedgecombe.com/");
-		getConfig().addDefault("", "Created by JasonFTW");
-		getConfig().addDefault(path, "");
+		//String path = "Settings.Items";
+		//getConfig().set(path, Arrays.asList(defaultItems));
+		//getConfig().addDefault("", "# Items support IDs only.");
+		//getConfig().addDefault("", "# Get ids from: http://minecraft-ids.grahamedgecombe.com/");
+		//getConfig().addDefault("", "# Created by JasonFTW");
+		//getConfig().addDefault(path, "");
 		getConfig().options().copyDefaults(true);
 		saveConfig();
 	}
 	
-	public String getItems(){
-		for(Object i : getConfig().getList("Settings.Items")){
-			String s = (String)i;
-			return s;
+	public int getItems(){
+		for(int i : getConfig().getIntegerList("Settings.Items")){
+			return i;
 		}
-		return null;
+		return -1;
 	}
 	
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void playerInteract(PlayerInteractEvent e){
-		if(e.getItem().equals(Material.getMaterial(getItems())) || e.getItem() != null || getItems() != null){
+		if(e.getItem().equals(Material.getMaterial(getItems())) || e.getItem() != null || getItems() != -1){
 			if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
 				getServer().getLogger().info("Player " + e.getPlayer() + " RIGHT CLICKED with item " + e.getItem());
 			}
